aeb4deada025466766316ca7ece9aa1b685a58b2
fmway <fm18lv@gmail.com>
2025-02-11 14:37:35 +0700

    feat: initial app

---
+++ app/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="font-bold text-4xl">PPBBMPV</Text>
+    </View>
+  );
+}
