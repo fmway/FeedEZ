1dbc23e583e3c9349c19ea13d13f07380a0e1b4e
fmway <fm18lv@gmail.com>
2025-03-24 12:19:06 +0700

    ...

--- backend/main.ts
+++ backend/main.ts
@@ -36,31 +36,47 @@ app
     const body = await c.req.json() as Partial<Setting>;
     if (body.duration) {
       setting.duration = body.duration;
+      devices.forEach(x => x.send("SET_DURATION, " + body.duration));
     }
     if (body.schedules) {
       setting.schedules.splice(0, setting.schedules.length);
       setting.schedules.push(...body.schedules);
+      const txt = ["SET_FEEDING", ...setting.schedules.map(x => `${x.hour}:${x.minute}`)].join(", ");
+      devices.forEach(x => x.send(txt));
     }
     if (body.speed) {
       setting.speed = body.speed;
+      devices.forEach(x => x.send("SET_SPEED, " + body.speed));
     }
+    clients.forEach(x => x.send(JSON.stringify({ cmd: "refresh" })));
     return c.json({ ok: true, message: "Successfully set" })
   })
   .get('/ws/client', (c, next) => {
     return upgradeWebSocket(() => ({
       onOpen(e, ws) {
-        setTimeout(() => {
-          clients.push(ws);
-          console.log("Connected clients: ", clients.length);
-        }, idleTimeout * 1000 + 100)
+        //setTimeout(() => {
+        clients.push(ws);
+        console.log("Connected clients: ", clients.length);
+        //}, idleTimeout * 1000 + 100)
       },
       onClose(e, ws) {
         clients.splice(clients.indexOf(ws), 1);
         console.log("Connected clients: ", clients.length);
       },
       onMessage(e, ws) {
+        const data = JSON.parse(e.data.toString());
         console.log("data from clients: ", e.data.toString());
-        devices.forEach(x => x.send(e.data.toString()))
+        devices.forEach(x => {
+          if (Object.hasOwn(data, "cmd") && typeof data.cmd === 'string') {
+            switch (data.cmd) {
+              case "getStatusRun":
+                x.send("GET_STATUS_RUN");
+                break;
+              default:
+                ;
+            }
+          } else x.send(data);
+        })
         isOnline = !isOnline;
         clients.filter(x => x != ws).forEach(x => x.send(`${isOnline}`));
       },
@@ -69,10 +85,10 @@ app
   .get('/ws/device', (c, next) => {
     return upgradeWebSocket(() => ({
       onOpen(e, ws) {
-        setTimeout(() => {
-          devices.push(ws);
-          console.log("Connected devices: ", devices.length);
-        }, idleTimeout * 1000 + 100)
+        //setTimeout(() => {
+        devices.push(ws);
+        console.log("Connected devices: ", devices.length);
+        //}, idleTimeout * 1000 + 100)
       },
       onClose(e, ws) {
         devices.splice(devices.indexOf(ws), 1);
@@ -80,6 +96,34 @@ app
       },
       onMessage(e, ws) {
         console.log("data from devices: ", e.data.toString());
+        const splitted = e.data.toString().split(",").map(x => x.toLowerCase().trim());
+        console.log("splitted to: ", JSON.stringify(splitted));
+        const cmd = splitted.shift();
+        console.log(JSON.stringify({ cmd, splitted }, null, 2));
+        switch (cmd) {
+          case "status_run":
+            isOnline = JSON.parse(splitted.at(0) || "false");
+            clients.forEach(x => x.send(JSON.stringify({ isOnline })));
+            break;
+          case "set_speed":
+            setting.speed = parseInt(splitted.at(0) || "1");
+            clients.forEach(x => x.send(JSON.stringify({ cmd: "refresh" })));
+            break;
+          case "set_feeding": {
+            setting.schedules.splice(0, setting.schedules.length);
+            setting.schedules.push(...splitted.map(x => {
+              const y = x.split(":").map(x => x.trim());
+              return {
+                hour: parseInt(y.at(0) || "0"),
+                minute: parseInt(y.at(1) || "0"),
+              };
+            }));
+            clients.forEach(x => x.send(JSON.stringify({ cmd: "refresh" })));
+            break;
+          }
+          default:
+            ;
+        }
       },
     }))(c, next)
   })
